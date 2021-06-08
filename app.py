8bf5b8d60891a975285580780061cb14db55554a
nsog01 <nsog01@uqo.ca>
2021-06-08 01:28:13 -0400

    Resources Template fie added

--- app.py
+++ app.py
@@ -20,10 +20,10 @@ def create_tables():
 
 jwt = JWT(app, authenticate, identity)  
 
-#api.add_resource(Store, '/store/<string:name>')
-#api.add_resource(StoreList, '/stores')
-#api.add_resource(Item, '/item/<string:name>')
-#api.add_resource(ItemList, '/items')
+#api.add_resource()
+#api.add_resource()
+#api.add_resource()
+#api.add_resource()
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
--- templates/Resources.tpl
+++ templates/Resources.tpl
@@ -1,6 +1,6 @@
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
-from models.item import ItemModel
+from models.{{}} import ItemModel
 
 
 class Item(Resource):
